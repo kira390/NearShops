d7c6381d306c75e5a3712e2975467e044a334b71
Othmane El Warrak <othmanelwarrak@gmail.com>
2018-06-30 17:10:31 +0100

    Setting up the API Gateway sub project
    creating the API Gateway subproject:
    - creating subproject template
    - Dockerfile to lunch the gateway as a container
    - Adding the API Gateway to the project docker-compose

---
+++ api_gateway/apigateway.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    return "Hello World, I'm the Gateway"
+
+if __name__ == '__main__':
+    app.run(port=80, host='0.0.0.0', debug=True)
